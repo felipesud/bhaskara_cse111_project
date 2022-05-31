dd28b756c8fee329b8c334fa9842b1016b1fe10e
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-05-31 19:31:25 -0300

    Add tkinter screen

---
+++ my_speeches.py
@@ -0,0 +1,29 @@
+import requests
+from tkinter import *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Tk()
+screen.title('Caçapava Ward Speeches invitation')
+orientation_text = Label(screen, text='Speech Invite')
+orientation_text.grid(column=0, row=0)
+
+screen.mainloop()
